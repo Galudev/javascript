8f733a2ad290ea4f8acf9227394c3bd8f73bec18
Lucia Galvez <lucia.galudev@gmail.com>
2022-06-08 17:16:55 +0200

    Solución Ejercicio 3

---
+++ Ejercicio3/main.js
@@ -0,0 +1,29 @@
+var nombre;
+nombre = "Lucía";
+console.log(nombre);
+console.log(typeof nombre);
+llamar();
+
+function llamar(){
+    var apellidos = "Gálvez Durán";
+    console.log(apellidos);
+    console.log(typeof apellidos);
+}
+
+let activo;
+activo = true;
+console.log(activo);
+console.log(typeof activo);
+noActivo();
+
+function noActivo(){
+    let activo = false;
+    console.log(activo);
+    console.log(typeof activo);
+}
+
+console.log(activo);
+
+const PI = 3.1415;
+console.log(PI);
+console.log(typeof PI);
