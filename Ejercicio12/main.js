86135b432acd3a034026f63f9956cb462d59ffa0
Lucia Galvez <lucia.galudev@gmail.com>
2022-06-09 13:31:15 +0200

    Solución Ejercicio 12

---
+++ Ejercicio12/main.js
@@ -0,0 +1,13 @@
+const array = ['a','b','c','d','e'];
+
+array.forEach(item => {
+    console.log(item);
+})
+
+let numero = 5;
+
+do{
+    console.log(numero--);
+} while(numero>0)
+
+console.log(numero)
