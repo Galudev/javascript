a7c1cf8c373dab7198cffa487b5a6102b89028cd
Lucia Galvez <lucia.galudev@gmail.com>
2022-06-09 13:00:19 +0200

    Solución Ejercicio 9

---
+++ Ejercicio9/main.js
@@ -0,0 +1,6 @@
+/* Número aleatorio (0 o 1): Se genera un número aleatorio entre 0 y 1 con Math.random() 
+y posteriormente se redondea con Math.round() para que quede 0 o 1 */
+console.log(Math.round(Math.random()));
+
+/* Redondear PI a 3: Usando el método Math.floor() introduciendo el valor Math.PI como valor */
+console.log(Math.floor(Math.PI));
